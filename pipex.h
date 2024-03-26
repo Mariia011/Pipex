ecf91d91403bbf8688cfa4f7fafc45e5ddb6aa60
Mariia Khachaturova <marikhac@c1r4s2.42yerevan.am>
2024-03-26 18:05:40 +0400

    first try

---
+++ pipex.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marikhac <marikhac@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/26 14:50:34 by marikhac          #+#    #+#             */
+/*   Updated: 2024/03/26 14:50:39 by marikhac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "./libft/libft.h"
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+#endif
